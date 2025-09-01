b508a3623ef8536f90c75cd69a37536e546d215b
ValientRH <shub32323@gmail.com>
2025-09-02 00:50:12 +0530

    Updated about section

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files (CSS, images) from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Route for the home page
 app.get('/', (req, res) => {
-    res.render('index', { name: "Shubham Kumar" });
+    res.render('index', { name: "Shubham kumar" });
 });
 
 // Start the server
